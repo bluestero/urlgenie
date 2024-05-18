7f5b5e2745f6888c2d59d445ac8902f067e53c43
Ahmed Khatib <ahmedkhatib99@gmail.com>
2024-05-18 12:58:10 +0530

    Updated LICENSE, setup.py, __init__.py, and setup.cfg files.

---
+++ setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+
+
+def get_description():
+    try:
+        with open("README.md", encoding = "utf-8") as readme_file:
+            long_description = readme_file.read()
+        return long_description
+    except:
+        return None
+
+setup(
+    license = "MIT",
+    name = "url-gen",
+    version = "1.0.0",
+    packages = ["url_gen"],
+    author = "Ahmed Khatib",
+    python_requires = ">=3.7",
+    maintainer = "Ahmed Khatib",
+    install_requires = ["tldextract"],
+    long_description = get_description(),
+    author_email = "ahmedkhatib99@gmail.com",
+    maintainer_email = "ahmedkhatib99@gmail.com",
+    long_description_content_type = "text/markdown",
+    description = "Tool to make URL extraction, generalization, validation, and filtration easy.",
+    project_urls={
+        "Documentation": "https://github.com/bluestero/url-gen/blob/main/README.md",
+        "Source": "https://github.com/bluestero/url-gen",
+        "Tracker": "https://github.com/bluestero/url-gen/issues",
+    },
+    keywords = [
+        "url-parsing",
+        "data-cleaning",
+        "data-curation",
+        "generalization",
+        "data-cleansing",
+        "data-processing",
+        "data-sanitization",
+        "url-generalization",
+    ],
+    classifiers = [
+        'Intended Audience :: Developers',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
--- url_gen/__init__.py
+++ url_gen/__init__.py
@@ -1,5 +1,5 @@
-__version__ = '1.1'
-__release__ = '2023-10-31'
+__version__ = '1.0.0'
+__release__ = '18-05-2024'
 
 
 from .url_gen import UrlGeneralizer
